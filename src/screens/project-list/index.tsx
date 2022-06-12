6a03edc9844754142309168e997272ca9845407e
yinchen <974578444@qq.com>
2022-06-12 23:06:11 +0800

    feat:开始JWT

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import * as qs from "qs";
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+import { cleanObject, useDebounce, useMount } from "utils";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const debouncedParam = useDebounce(param, 2000);
+  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List list={list} users={users}></List>
+    </div>
+  );
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,28 +1,30 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 // 在一个函数里，改变传入的对象本身是不好的
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   if (!object) {
     return {};
   }
   const result = { ...object };
-  Object.keys(result).forEach((key) => {
+  Object.keys(result).forEach((key: string) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: unknown, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
