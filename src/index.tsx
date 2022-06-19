5bf809abd142e328c6d1fb77c92d2be14a790a9c
yinchen <974578444@qq.com>
2022-06-19 23:55:36 +0800

    feat: 开始jwt

--- src/index.tsx
+++ src/index.tsx
@@ -9,12 +9,16 @@ import { loadDevTools } from "jira-dev-tool";
 //   document.getElementById("root") as HTMLElement
 // );
 // root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-ReactDOM.render(<App />, document.getElementById("root"));
 
+// );
+loadDevTools(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
