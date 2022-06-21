b582568d6dd0540482aa90fadaf54292ab263416
yinchen <974578444@qq.com>
2022-06-21 23:55:48 +0800

    feat: 完成auth-context

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,15 +1,38 @@
 import React, { useState } from "react";
+import * as auth from "auth-provider";
+import { User } from "screens/project-list/search-panel";
 
 interface AutoForm {
   username: string;
   password: string;
 }
 
-const AuthContext = React.createContext(undefined);
+const AuthContext = React.createContext<
+  | {
+      user: User | null;
+      register: (form: AutoForm) => Promise<void>;
+      login: (form: AutoForm) => Promise<void>;
+      logout: () => Promise<void>;
+    }
+  | undefined
+>(undefined);
 AuthContext.displayName = "AuthContext";
 
 export const AuthContext = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<User | null>(null);
 
-  const;
+  //point free
+  const login = (form: AutoForm) => auth.login(form).then(setUser);
+  const register = (form: AutoForm) => auth.register(form).then(setUser);
+  const logout = () => auth.logout().then(() => setUser(null));
+
+  return <AuthContext.Provider value={{ user, login, register, logout }} />;
+};
+
+export const useAuth = () => {
+  const context = React.useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth必须在AuthProvider中使用");
+  }
+  return context;
 };
